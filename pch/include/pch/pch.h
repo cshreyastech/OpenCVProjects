6e2081b07731ed91a500598c9d5ae89d60a05f0a
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2023-10-31 09:31:22 -0700

    Validated Opencv initial code.

---
+++ pch/include/pch/pch.h
@@ -0,0 +1,22 @@
+#ifndef _PCH_
+#define _PCH_
+
+#include <iostream>
+#include <stdio.h>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <cassert>
+#include <exception>
+#include <iomanip>
+#include <limits>
+#include <ctime>
+#include <algorithm> // copy(), assign()
+#include <iterator> // back_inserter
+#endif
